8fb1bfba3fe8c5b3d3be9c4a2efcf7fc7ce4eda9
Bruce Ng <bng@yahoo-inc.com>
2015-01-28 22:03:20 -0800

    Add more data to business data model

--- Yelp/Business.h
+++ Yelp/Business.h
@@ -13,8 +13,22 @@
 +(NSArray *) businessesWithDictionaries:(NSArray *)businessDictionaries;
 
 @property (nonatomic, strong) NSString* name;
-@property (nonatomic, strong) NSArray* address;
 @property (nonatomic, strong) NSArray* categories;
+@property (nonatomic, strong) NSString* display_phone;
+@property (nonatomic) float distance;
+@property (nonatomic, strong) NSString* image_url;
+@property (nonatomic) BOOL is_claimed;
+@property (nonatomic) BOOL is_closed;
+@property (nonatomic, strong) NSArray* address;
+@property (nonatomic, strong) NSString* city;
+@property (nonatomic, strong) NSDictionary* coordinate;
+@property (nonatomic, strong) NSArray* neighborhoods;
+@property (nonatomic) float rating;
+@property (nonatomic) NSInteger phone;
+@property (nonatomic, strong) NSString* rating_img_url;
+@property (nonatomic, strong) NSString* rating_img_url_large;
+@property (nonatomic, strong) NSString* rating_img_url_small;
+@property (nonatomic, strong) NSString* url;
 
 
 
--- Yelp/Business.m
+++ Yelp/Business.m
@@ -15,8 +15,20 @@
     for (NSDictionary *businessDict in businessDictionaries) {
         Business *business = [[Business alloc] init];
         business.name = businessDict[@"name"];
-        business.address = [businessDict valueForKeyPath:@"location.address"];
         business.categories = businessDict[@"categories"];
+        business.display_phone = businessDict[@"display_phone"];
+        business.distance = [businessDict[@"distance"] floatValue];
+        business.image_url = businessDict[@"image_url"];
+        business.is_claimed = [businessDict[@"is_claimed"] boolValue];
+        business.is_closed = [businessDict[@"is_closed"] boolValue];
+        business.address = [businessDict valueForKeyPath:@"location.address"];
+        business.city = [businessDict valueForKey:@"location.city"];
+        business.coordinate = [businessDict valueForKey:@"location.coordinate"];
+        business.rating = [businessDict[@"distance"] floatValue];
+        business.rating_img_url = businessDict[@"rating_img_url"];
+        business.rating_img_url_large = businessDict[@"rating_img_url_large"];
+        business.rating_img_url_small = businessDict[@"rating_img_url_small"];
+        business.url = businessDict[@"url"];
         [businesses addObject:business];
         
     }
